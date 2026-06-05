8735ce4a33a30687f53f3824fc9b820fc2cc2202
ArghoPH <arghochakma2023@gmail.com>
2026-06-06 00:19:35 +0600

    fixed theme bugs

--- src/Components/ThemeToggle.vue
+++ src/Components/ThemeToggle.vue
@@ -1,18 +1,25 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import { toggleTheme, getInitialTheme, applyTheme } from '@/utils/theme.js'
 
 const current = ref(getInitialTheme())
-applyTheme(current.value)
+
+onMounted(() => {
+  applyTheme(current.value)
+})
 
 function handleToggle() {
   current.value = toggleTheme()
 }
+
+watch(current, (newTheme) => {
+  applyTheme(newTheme)
+}, { immediate: true })
 </script>
 
 <template>
-  <button @click.stop="handleToggle" :aria-pressed="current === 'dark'" class="p-2 rounded-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-100 focus:outline-none">
-    <i v-if="current === 'dark'" class="fa-solid fa-sun"></i>
-    <i v-else class="fa-solid fa-moon"></i>
+  <button @click.stop="handleToggle" :aria-pressed="current === 'dark'" class="p-2 rounded-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-100 focus:outline-none hover:bg-gray-300 dark:hover:bg-gray-600 transition">
+    <i v-if="current === 'dark'" class="fa-solid fa-sun text-lg"></i>
+    <i v-else class="fa-solid fa-moon text-lg"></i>
   </button>
 </template>
