434c3c4b41f74cce75d1e7eb7ebd9459417003c2
ArghoPH <arghochakma2023@gmail.com>
2026-06-06 14:52:00 +0600

    fixing bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-module.exports = {
-  darkMode: 'class',
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+
+export default {
+    darkMode: 'class',
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
