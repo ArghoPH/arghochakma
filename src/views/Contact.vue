1e44d4698bd7cac7ec1cb3a54f6b62246d272fa0
ArghoPH <arghochakma2023@gmail.com>
2026-06-12 12:47:11 +0600

    added changes

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -51,7 +51,7 @@ function handleSubmit() {
                     <textarea rows="4" placeholder="Your Message"
                         class="rounded-xl px-5 py-3 focus:outline-none"></textarea>
                     <button class="btn-neon text-white font-semibold px-6 py-4 rounded-xl">
-                        Send Message 🚀
+                        Send Message for more information
                     </button>
                 </div>
             </div>
