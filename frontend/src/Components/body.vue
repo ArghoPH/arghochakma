2b5635b32a57af4cf6a65e10034c829569852029
ArghoPH <arghochakma2023@gmail.com>
2026-06-23 10:52:56 +0600

    com

--- frontend/src/Components/body.vue
+++ frontend/src/Components/body.vue
@@ -63,12 +63,12 @@ onBeforeUnmount(() => {
                     </div>
 
                     <!-- Right Arrow -->
-                    <div class="mt-4 animate-slide-x flex justify-start items-center gap-2">
+                    <!-- <div class="mt-4 animate-slide-x flex justify-start items-center gap-2">
                         <div
                             class="bg-yellow-300 text-gray-900 px-4 py-2 rounded-full cursor-pointer select-none font-bold">
                             <p class="text-gray-700">The Skills ❖</p>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
 
                 <!-- Right -->
